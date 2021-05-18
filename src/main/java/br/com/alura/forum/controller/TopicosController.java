93f34f25c87da4b3e40324df4d38f177c2db51ed
Bruna Mello <brunacrespomello@gmail.com>
2021-05-18 04:43:23 +0100

    06. Methodos PUT, DELETE e tratamento de erros 404
    Nesta aula, aprendemos que:
    
    - Para receber parâmetros dinâmicos no path da URL, devemos utilizar a anotação @PathVariable;
    - Para mapear requisições do tipo PUT, devemos utilizar a anotação @PutMapping;
    - Para fazer o controle transacional automático, devemos utilizar a anotação @Transactional nos métodos do controller;
    - Para mapear requisições do tipo DELETE, devemos utilizar a anotação @DeleteMapping;
    - Para tratar o erro 404 na classe controller, devemos utilizar o método findById, ao invés do método getOne, e utilizar a classe ResponseEntity para montar a resposta de not found;
    - O método getOne lança uma exception quando o id passado como parâmetro não existir no banco de dados;
    - O método findById retorna um objeto Optional<>, que pode ou não conter um objeto.

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,18 +1,22 @@
 package br.com.alura.forum.controller;
 
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
+import br.com.alura.forum.dto.DetalhesTopicoDto;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController //já devolve a pagina como entidade nao precisando mais do response body
 @RequestMapping("/topicos")
@@ -29,22 +33,69 @@ public class TopicosController {
 
         if (nomeCurso == null) {
             List<Topico> topicos = topicoRepository.findAll();
+
             return TopicoDto.converter(topicos);
         } else {
             List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+
             return TopicoDto.converter(topicos);
         }
 
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
 
         //fazendo o retorno especifico 200.
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+
         return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+
+    @GetMapping("/{id}") //url dinamica
+    public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
+        //tratamento para parametro id invalido
+        Optional<Topico> topico  = topicoRepository.findById(id);
+
+        if (topico.isPresent()) {
+            return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    //put muda todos os campos e path muda somente um
+    @PutMapping("/{id}")
+    @Transactional //para commitar a transacao
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,@RequestBody @Valid AtualizacaoTopicoForm form) {
+        Optional<Topico> optional  = topicoRepository.findById(id);
+
+        if (optional.isPresent()) {
+            Topico topico = form.atualizar(id, topicoRepository);
+            return ResponseEntity.ok(new TopicoDto(topico));
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+
+        Optional<Topico> optional  = topicoRepository.findById(id);
+
+        if (optional.isPresent()) {
+            topicoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+
     }
 
 
