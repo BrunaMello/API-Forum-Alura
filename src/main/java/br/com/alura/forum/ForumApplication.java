80f48a242b94a5787034005388e1445796d96d96
brunamello <brunacrespomello@gmail.com>
2021-05-29 05:33:22 +0100

    02. Melhorando desempenho com Spring Cache
    Nesta aula, aprendemos que:
    
    - Para utilizar o módulo de cache do Spring Boot, devemos adicioná-lo como dependência do projeto no arquivo pom.xml;
    - Para habilitar o uso de caches na aplicação, devemos adicionar a anotação @EnableCaching na classe ForumApplication;
    - Para que o Spring guarde o retorno de um método no cache, devemos anotá-lo com @Cacheable;
    - Para o Spring invalidar algum cache após um determinado método ser chamado, devemos anotá-lo com @CacheEvict;
    - Devemos utilizar cache apenas para as informações que nunca ou raramente são atualizadas no banco de dados.

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,10 +2,14 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import javax.persistence.Cacheable;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport //habilitando a paginacao
+@EnableCaching
 public class ForumApplication {
 
     public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,8 +8,9 @@ import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -20,7 +21,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 @RestController //já devolve a pagina como entidade nao precisando mais do response body
@@ -35,6 +35,7 @@ public class TopicosController {
 
     //quando é get e tem parametro, tem que colocar que vai na request: @RequestParam
     @GetMapping
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
                                  @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable paginacao) {
 
@@ -47,11 +48,11 @@ public class TopicosController {
 
             return TopicoDto.converter(topicos);
         }
-
     }
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -78,6 +79,7 @@ public class TopicosController {
     //put muda todos os campos e path muda somente um
     @PutMapping("/{id}")
     @Transactional //para commitar a transacao
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,@RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional  = topicoRepository.findById(id);
 
@@ -92,6 +94,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
 
         Optional<Topico> optional  = topicoRepository.findById(id);
@@ -104,8 +107,6 @@ public class TopicosController {
         return ResponseEntity.notFound().build();
 
     }
-
-
 }
 
 
